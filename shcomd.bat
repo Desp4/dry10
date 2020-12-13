9b7daac5f0ba2bd6073ce4a108deefae815da414
Desp4 <desp4.kot@gmail.com>
2020-12-14 02:09:18 +0300

    add linux config, cleanup

--- shcomd.bat
+++ shcomd.bat
@@ -1,13 +1,18 @@
 @echo OFF
-rem %1 - processing dir %2 - glslc path %3 spir-v dir %4 pcsr path %5 out header dir
+rem %1 - processing dir %2 spir-v dir %3 pcsr path %4 out header dir
 
 for %%f in (%1/*.vert) do (
-    echo compiling shader %%~nf
+    echo generating shader %%~nf
+
     echo compiling %%~nf.vert
-    %2 %1/%%~nf.vert -o %3/%%~nf.vert.spv
+    glslc.exe %1/%%~nf.vert -o %2/%%~nf.vert.spv || exit /b
+    echo %%~nf.vert written to %2/%%~nf.vert.spv
+
     echo compiling %%~nf.frag
-    %2 %1/%%~nf.frag -o %3/%%~nf.frag.spv
-    
-    echo generating headers
-    %4 %3/%%~nf.vert.spv %3/%%~nf.frag.spv %%~nf %5
+    glslc.exe %1/%%~nf.frag -o %2/%%~nf.frag.spv || exit /b
+    echo %%~nf.frag written to %2/%%~nf.frag.spv
+
+    echo launching pcsr for %%~nf
+    %3 %2/%%~nf.vert.spv %2/%%~nf.frag.spv %%~nf %4 || exit /b
+    echo %%~nf generated
 )
